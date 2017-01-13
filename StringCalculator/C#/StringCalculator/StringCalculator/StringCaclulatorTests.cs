70566cf48373a3a8ffb63936d054de405846c8eb
Fyzxs <Fyzxs@users.noreply.github.com>
2017-01-13 05:44:46 -0800

    String Calculator

---
+++ StringCalculator/C#/StringCalculator/StringCalculator/StringCaclulatorTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculator
+{
+    [TestClass]
+    public class StringCaclulatorTests
+    {
+        private readonly StringSum _s = new StringSum();
+        [TestMethod]
+        public void EmptyString()
+        {
+            Assert.AreEqual(0, _s.Sum(""));
+        }
+
+        [TestMethod]
+        public void SingleDigit()
+        {
+            Assert.AreEqual(3, _s.Sum(3.ToString()));
+        }
+
+        [TestMethod]
+        public void TwoDigitsComma()
+        {
+            Assert.AreEqual(5, _s.Sum("3,2"));
+        }
+
+        [TestMethod]
+        public void TwoDigitsNewLine()
+        {
+            Assert.AreEqual(5, _s.Sum("3\n2"));
+        }
+
+        [TestMethod]
+        public void ThreeDigitsEitherDelimiter()
+        {
+            Assert.AreEqual(9, _s.Sum("3\n2,4"));
+        }
+
+        [TestMethod]
+        public void UnknownNumbersEitherDelim()
+        {
+            int rnd = new Random().Next(5, 20);
+            StringBuilder sb = new StringBuilder();
+            int sum = 0;
+            for (int i = 1; i < rnd; i++)
+            {
+                sum += i;
+                sb.Append(i);
+                sb.Append(i%2 == 0 ? ',' : '\n');//Alternating delimiter
+            }
+            sb.Remove(sb.Length - 1, 1);
+            Assert.AreEqual(sum, _s.Sum(sb.ToString()));
+        }
+
+        [TestMethod]
+        public void ConfigurableDelimiter()
+        {
+            Assert.AreEqual(6, _s.Sum("//;\n1;2;3"));
+        }
+
+        [TestMethod]
+        public void NegativeThrows()
+        {
+            try
+            {
+                _s.Sum("-13");
+                Assert.Fail("Exception Not Thrown");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.IsTrue(e.Message.Contains("-13"));
+            }
+        }
+        [TestMethod]
+        public void NegativeThrowContainsAllNegatives()
+        {
+            try
+            {
+                _s.Sum("-13,1,-12");
+                Assert.Fail("Exception Not Thrown");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.IsTrue(e.Message.Contains("-13,-12"));
+            }
+        }
+
+    }
+}
---
+++ StringCalculator/C#/StringCalculator/StringCalculator/StringSum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculator
+{
+    internal class StringSum
+    {
+        public int Sum(string numberString)
+        {
+            if (IsEmpty(numberString)) return 0;
+            var numbers = SplitNumbers(numberString);
+            ThrowOnNegatives(numbers);
+            return numbers.Sum(int.Parse);
+        }
+
+        private static void ThrowOnNegatives(string[] numbers)
+        {
+
+            var negatives = numbers.Where(n => int.Parse(n) < 0).ToList();
+            if (!negatives.Any()) return;
+
+            throw new ArgumentException($"Negative Values [{string.Join(",", negatives)}] not allowed");
+        }
+
+        private static string[] SplitNumbers(string numberString)
+        {
+            if (!numberString.StartsWith("//")) return numberString.Split(',', '\n');
+            
+            int firstLineEnd = numberString.IndexOf('\n');
+            string delim = numberString.Substring(2, 1);
+            return numberString.Substring(firstLineEnd + 1).Split(delim.ToCharArray());
+        }
+
+        private static bool IsEmpty(string numberString)
+        {
+            return numberString == string.Empty;
+        }
+    }
+}
